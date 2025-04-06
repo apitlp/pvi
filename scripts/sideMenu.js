bd21706dfc0f449b9baca4aeaf5d45f44393622b
apitlp <andriipotikha05@gmail.com>
2025-04-06 20:49:19 +0300

    delete before lab3

--- scripts/sideMenu.js
+++
@@ -1,6 +0,0 @@
-const burgerMenu = document.querySelector(".burger-menu");
-const navBar = document.querySelector("nav");
-
-burgerMenu.addEventListener("click", () => {
-    navBar.style.display = (navBar.style.display === "block") ? "none" : "block";
-});
